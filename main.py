43b70796e8181f86c93a1beda2350ac6618e22f5
Amelia <amelia.ulatowska.27@gmail.com>
2024-10-23 21:14:42 +0200

    draft

--- main.py
+++ main.py
@@ -1,1 +1,45 @@
-wow
+class Node:
+    def __init__(self, val):
+        self.v = val
+        self.l = None
+        self.r = None
+
+class BST:
+    def __init__(self):
+        self.root = None
+    
+    def member(self, val):
+        return NotImplementedError
+    
+    def insert(self, val):
+        return NotImplementedError
+    
+    def delete(self, val):
+        return NotImplementedError
+    
+    def JestOswietlona(self):
+        return NotImplementedError
+    
+    def JestStylowa(self):
+        return NotImplementedError
+    
+    def JestStabilna(self):
+        return NotImplementedError
+    
+    def NajdluzszyLancuchKolorowy(self):
+        return NotImplementedError
+    
+    def JestElegancka(self):
+        return NotImplementedError
+    
+    def JestTradycyjna(self):
+        return NotImplementedError
+    
+    def IleLancuchowJednobarwnych(self):
+        return NotImplementedError
+
+    def JestGotowa(self):
+        return NotImplementedError
+
+def GenRandBST(n):
+    return NotImplementedError
