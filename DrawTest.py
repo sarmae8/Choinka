0ae2e7dcfa6d2ae0f3ae22681c8028a0d63bb4a3
Antoni Marczuk <antoni.marczuk123@gmail.com>
2024-10-29 16:30:05 +0100

    Dodano moduł DrawTest.py

---
+++ DrawTest.py
@@ -0,0 +1,17 @@
+from BST import *
+
+def main():
+    bst : BST = BST()
+    bst.insert(5)
+    bst.insert(3)
+    bst.insert(7)
+    bst.insert(8)
+    bst.insert(6)
+    bst.insert(4)
+    bst.insert(2)
+    bst.insert(1)
+
+    bst.display(scale = 150)
+
+if __name__ == "__main__":
+    main()
