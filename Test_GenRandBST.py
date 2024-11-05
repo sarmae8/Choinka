f8e872b644307ee837026c37868d80772f7d572b
Amelia <amelia.ulatowska.27@gmail.com>
2024-11-05 15:59:27 +0100

    nic waznego

--- Test_GenRandBST.py
+++ Test_GenRandBST.py
@@ -5,6 +5,15 @@ from GenRandBST import *
 def main():
     bst = GenRandBST(30)
     bst.display()
+    print(f"Ile łańcuchów jednobarwnych: {bst.IleLancuchowJednobarwnych()}")
+    print(f"Jest elegancka: {bst.JestElegancka()}")
+    print(f"Jest gotowa: {bst.JestGotowa()}")
+    print(f"Jest oświetlona: {bst.JestOswietlona()}")
+    print(f"Jest równo oświetlona: {bst.JestRownoOswietlona()}")
+    print(f"Jest stablina {bst.JestStabilna()}")
+    print(f"Jest stylowa: {bst.JestStylowa()}")
+    print(f"Jest tradycyjna: {bst.JestTradycyjna()}")
+    print(f"Najdłuższy łańcuch kolorowy: {bst.NajdluzszyLancuchKolorowy()}")
 
 if __name__ == "__main__":
     main()
